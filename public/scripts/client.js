35a2d7c3b55ab0d1e744698d21c764f528720599
kyl graym <kylemaxgray@gmail.com>
2021-09-09 21:29:39 +0000

    added simple form validation and XSS protection

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,26 @@
 $(() => {
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
   // Tweet <article> template
   const createTweetElement = (tweet) => {
     const $tweet = 
       `<article>
         <header>
           <div class="user-icon-name">
-            <img src="${tweet.user.avatars}" class="fas fa-user-astronaut" />
-            <h2>${tweet.user.name}</h2>
+            <img src="${escape(tweet.user.avatars)}" class="fas fa-user-astronaut" />
+            <h2>${escape(tweet.user.name)}</h2>
           </div>
           <div class="user-tag">
-            <h3>${tweet.user.handle}</h3>
+            <h3>${escape(tweet.user.handle)}</h3>
           </div>
         </header>
         <div class="post-content">
-        <p>${tweet.content.text}</p>
+        <p>${escape(tweet.content.text)}</p>
         </div>
         <div class="div-line"></div>
         <footer class="article-footer">
@@ -35,24 +41,41 @@ $(() => {
   }
 
   const renderTweets = function(tweets) {
-    for (let key in tweets) {
-      $('#tweets-container').prepend(createTweetElement(tweets[key]));
+    const $tweetsContainer = $('#tweets-container');
+    $tweetsContainer.empty()
+    for (let tweet in tweets) {
+      $('#tweets-container').prepend(createTweetElement(tweets[tweet]));
     }
   }
 
+  // queries the db for tweets
   const loadTweets = function () {
     $.get('/tweets', response => {
       renderTweets(response);
     });
   };
 
-  // grabs form data and posts to $tweets
 
+  // on submit posts tweet and reloads tweets
   $('form').submit(function(event) {
-    
     event.preventDefault();
     const $form = $(this);
+
+    console.log('what the form looks like: ', $form);
     const data = $form.serialize();
+
+    const rawInput = $(this).find('input').val();
+    console.log('raw input ', rawInput);
+
+    if (rawInput.length > 140) {
+      alert('You can only do 140 characters');
+      return false;
+    }
+    if (rawInput === '' || rawInput === null){
+      alert('you cant submit nothing');
+      return false;
+    }
+
     // AJAX
     $.post("/tweets", data, res => {
       console.log(res);
@@ -60,10 +83,5 @@ $(() => {
     });
   });
 
-
-
   loadTweets()
-
-
-
 });
