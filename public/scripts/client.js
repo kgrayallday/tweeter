fdf8fb6f616e0015b8739e96fa208e4ee28486c5
kyl graym <kylemaxgray@gmail.com>
2021-09-09 20:28:27 +0000

    omplimented posting functionality

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,69 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+$(() => {
 
+  // Tweet <article> template
+  const createTweetElement = (tweet) => {
+    const $tweet = 
+      `<article>
+        <header>
+          <div class="user-icon-name">
+            <img src="${tweet.user.avatars}" class="fas fa-user-astronaut" />
+            <h2>${tweet.user.name}</h2>
+          </div>
+          <div class="user-tag">
+            <h3>${tweet.user.handle}</h3>
+          </div>
+        </header>
+        <div class="post-content">
+        <p>${tweet.content.text}</p>
+        </div>
+        <div class="div-line"></div>
+        <footer class="article-footer">
+          <div class="post-age">
+            <h6>${timeago.format(tweet.created_at)}</h6>
+          </div>
+          <div class="spacer"></div>
+          <div class="interact">
+            <i class="fas fa-flag"></i>
+            <i class="fas fa-retweet"></i>
+            <!-- <i class="fas fa-recycle"></i> -->
+            <i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>`;
+    return $tweet;
+
+  }
+
+  const renderTweets = function(tweets) {
+    for (let key in tweets) {
+      $('#tweets-container').prepend(createTweetElement(tweets[key]));
+    }
+  }
+
+  const loadTweets = function () {
+    $.get('/tweets', response => {
+      renderTweets(response);
+    });
+  };
+
+  // grabs form data and posts to $tweets
+
+  $('form').submit(function(event) {
+    
+    event.preventDefault();
+    const $form = $(this);
+    const data = $form.serialize();
+    // AJAX
+    $.post("/tweets", data, res => {
+      console.log(res);
+      loadTweets()
+    });
+  });
+
+
+
+  loadTweets()
+
+
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
   // --- our code goes here ---
 
   const $tweetText = $('#tweet-text');
+  
 
-  $tweetText.on('keyup', (event) => {
+  $tweetText.on('keyup', function (event) {
     
-    const $charCounter = $(".counter");
+    const myInput = $(this)[0].value;
+    const $charCounter = $(this).parent().find('output');
     const chars = event.target.value.length;
 
     const maxChars = 140;
@@ -19,4 +21,5 @@ $(document).ready(function() {
       $charCounter.css('color', 'blue');
     }
   });
+
 });
