a61571bea22ad77ac08a30653accde8110c5f21f
kyl graym <kylemaxgray@gmail.com>
2021-09-14 19:29:40 +0000

    fix posting whitespace, set timer to slideup and clear post

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,6 +33,12 @@ $(() => {
     return $tweet;
   }
 
+  // auto size textarea input
+  $('#tweet-text').on('input', function () {
+    this.style.height = 'auto';
+    this.style.height = (this.scrollHeight) + 'px';
+  });
+
   // (re)draws tweet article to tweet container div
   const renderTweets = function(tweets) {
     const $tweetsContainer = $('#tweets-container');
@@ -56,25 +62,39 @@ $(() => {
     const $form = $(this);
     const data = $form.serialize();
     const rawInput = $(this).find('textarea').val();
+  
+    if (rawInput && !rawInput.trim()) {
+      $('.error-msg')
+      .html("<p> You cannot post whitespace only </p>")
+      .slideDown(700, function() {
+        $('.error-msg').delay(6000).slideUp(700);
+      });
+      $(this).trigger('reset');
+      return false;
+    }
 
     if (rawInput.length > 140) {
       $('.error-msg')
         .html("<p> Post must be under <u>140</u> characters.</p>")
-        .slideDown('slow')
+        .slideDown(700, function() {
+          $('.error-msg').delay(6000).slideUp(700);
+        });
+        $(this).trigger('reset');
       return false;
     }
 
     if (rawInput === '' || rawInput === null){
-      // $('.error-msg').css("visibility", "visible");
       $('.error-msg')
         .html("<p>you have to post <em>something!<em></p>")
-        .slideDown('slow')
+        .slideDown(700, function() {
+          $('.error-msg').delay(6000).slideUp(700);
+        });
+        $(this).trigger('reset');
       return false;
     }
 
     // AJAX POST
     $.post("/tweets", data, res => {
-      console.log(res);
       $('.error-msg').slideUp();
       $(this).trigger('reset');
       loadTweets()
@@ -83,6 +103,7 @@ $(() => {
   loadTweets()
 });
 
+// prevent XSS / INJECTION
 const escape = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
